02a10d74d910763b443cc433ff2038c879ad3119
Gene Blanchard <gblanchard4@gmail.com>
2016-11-01 21:56:31 -0500

    add PROGMEM fn's back in

--- keyboards/ergodox/keymaps/odwp/keymap.c
+++ keyboards/ergodox/keymaps/odwp/keymap.c
@@ -148,22 +148,13 @@ enum function_id {
     RSFT_2_CAP,
 };
 
-// const uint16_t PROGMEM fn_actions[] = {
-//     [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),        // FN1  = Momentary Layer 1 (Symbols)
-//     [2] = ACTION_FUNCTION(LSFT_2_CAP),          // FN29 = Toggle CapsLock if both Shifts hit
-//     [3] = ACTION_FUNCTION(RSFT_2_CAP),          // FN30 = Toggle CapsLock if both Shifts hit
-//     [4] = ACTION_LAYER_MOMENTARY(1),            // FN4  = Momentary Layer to use with F* keys on top row
-//     [5] = ACTION_LAYER_MOMENTARY(2),            // FN5  = Momentary to use with F* keys on top row + util
-//     [6] = ACTION_MODS_KEY(KC_LSFT, KC_SCLN),    // FN1  = Shifted SemiColon // : in Workman
-// };
-
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_MOMENTARY(1),        // FN1  = Momentary Layer 1 (Symbols)
-    [2] = ACTION_LAYER_MOMENTARY(2),          // FN29 = Toggle CapsLock if both Shifts hit
-    [3] = ACTION_LAYER_MOMENTARY(1),          // FN30 = Toggle CapsLock if both Shifts hit
-    [4] = ACTION_LAYER_MOMENTARY(2),            // FN4  = Momentary Layer to use with F* keys on top row
-    [5] = ACTION_LAYER_MOMENTARY(1),            // FN5  = Momentary to use with F* keys on top row + util
-    [6] = ACTION_LAYER_MOMENTARY(1),    // FN1  = Shifted SemiColon // : in Workman
+    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),        // FN1  = Momentary Layer 1 (Symbols)
+    [2] = ACTION_FUNCTION(LSFT_2_CAP),          // FN29 = Toggle CapsLock if both Shifts hit
+    [3] = ACTION_FUNCTION(RSFT_2_CAP),          // FN30 = Toggle CapsLock if both Shifts hit
+    [4] = ACTION_LAYER_MOMENTARY(1),            // FN4  = Momentary Layer to use with F* keys on top row
+    [5] = ACTION_LAYER_MOMENTARY(2),            // FN5  = Momentary to use with F* keys on top row + util
+    [6] = ACTION_MODS_KEY(KC_LSFT, KC_SCLN),    // FN1  = Shifted SemiColon // : in Workman
 };
 
 
